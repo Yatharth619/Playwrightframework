8c9b62f77a1d30b39831b4c9a93e8e17e8de4884
Yatharth619 <eldrago24567@gmail.com>
2026-07-27 16:50:24 +0530

    changed headless to true

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   use: {
     
     baseURL: 'https://www.saucedemo.com',
-    headless: false,
+    headless: true,
 
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
