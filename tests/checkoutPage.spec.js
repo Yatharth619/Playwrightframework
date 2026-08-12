f40874057bf3ea24611236ec13c7f02dc1226685
Yatharth619 <eldrago24567@gmail.com>
2026-08-12 18:49:15 +0530

    check for failure

--- tests/checkoutPage.spec.js
+++ tests/checkoutPage.spec.js
@@ -5,7 +5,7 @@ import { users } from "../data/logindata";
 test ("checkout page", async({inventoryPage, cartPage, checkoutPage}) => {
     
     await inventoryPage.navigate()
-    await inventoryPage.addProductToCart("Sauce Labs Bike Light")
+    await inventoryPage.addProductToCart("Saucessss Labs Bike Light")
     await inventoryPage.cartClick()
     await cartPage.checkout()
     await checkoutPage.enterCheckoutInfo(users.checkoutInfo.firstname, users.checkoutInfo.lastname, users.checkoutInfo.postalcode)
